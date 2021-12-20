661b1a7d1b9153f5cf3b9de77c7eaf09c703d1fa
waheed2021skipq <96059080+waheed2021skipq@users.noreply.github.com>
2021-12-20 15:15:05 +0500

    Update dynamodb_lambda.py

--- waheed_ahmad/sprint1/myproject/resources/dynamodb_lambda.py
+++ waheed_ahmad/sprint1/myproject/resources/dynamodb_lambda.py
@@ -1,2 +1,45 @@
 def lambda_handler(events,context):
+        db= boto3.resource('dynamodb')
+        
+        
+        
+        
+        
+        # msg = events[''][0]['sns']
+        # time = events[''][0]['sns']['time.datetime.timestamp']
+        # reasontoalarm= events['record'][0]['sns']
+        
+        
+        
     
+        # table = db.create_table(
+        #     TableName='alarmtable',
+        #     KeySchema=[
+        #         {
+        #             'AttributeName': 'alarmID',
+        #             'KeyType': 'HASH'  
+        #         },
+        #         {
+        #             'AttributeName': 'alarm',
+        #             'KeyType': 'RANGE'  
+        #         }
+        #         ],
+        #     AttributeDefinitions=[
+        #         {
+        #             'AttributeName': 'alarmID',
+        #             'AttributeType': 'N'
+        #         },
+        #         {
+        #             'AttributeName': 'Alarmtitle',
+        #             'AttributeType': 'S'
+        #         },
+
+        #                         ],
+        #     ProvisionedThroughput={
+        #         'ReadCapacityUnits': 10,
+        #         'WriteCapacityUnits': 10
+        #     }
+        # return table
+        # )     
+        
+        #db.table.grant_read_write_data(db_lambda)
