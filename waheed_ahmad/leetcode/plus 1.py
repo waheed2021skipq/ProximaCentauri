d2648287f717881632aad258a656864a98cce70b
waheed2021skipq <96059080+waheed2021skipq@users.noreply.github.com>
2021-12-26 09:59:15 +0500

    Create plus 1.py

---
+++ waheed_ahmad/leetcode/plus 1.py
@@ -0,0 +1,15 @@
+
+class Solution(object):
+    def plusOne(self, digits):
+      
+        for i in reversed(range(len(digits))):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        digits[0] = 1
+        digits.append(0)
+        return digits
+
+
