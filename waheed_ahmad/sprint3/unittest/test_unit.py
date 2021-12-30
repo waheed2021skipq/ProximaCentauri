44f3bfa17e87c9147778f7f9e66625420ae87e31
Ubuntu <ubuntu@ip-172-31-36-162.us-east-2.compute.internal>
2021-12-30 06:02:07 +0000

    unittest

--- waheed_ahmad/sprint3/unittest/test_unit.py
+++ waheed_ahmad/sprint3/unittest/test_unit.py
@@ -1,9 +1,9 @@
 import pytest
 from aws_cdk import core
-from pcwaheedproject2.pcwaheedproject2_stack import PcwaheedprojectStack
+from sprint3.sprint3_stack import waheedsprint2
 def test_lambda_stack():
     app = core.App()
-    PcwaheedprojectStack(app,"waheedteststack")
+    waheedsprint2(app,"waheedteststack")
     template=app.synth().get_stack_by_name('waheedteststack').template
     functions=[resource for resource in template['Resources'].values() if resource['Type']== 'AWS::Lambda::Function']
     assert len(functions)== 2
